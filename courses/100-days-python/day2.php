8945d29f1cfffd8154d7e97236bc569b5e636b7f
Arhan Ansari <arhanansari2009@gmail.com>
2024-06-06 18:03:59 +0000

    Add day 2 of 100 days of python

---
+++ courses/100-days-python/day2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+	<title>Day 2 - My Python Success Story - CodeWithArhan</title>
+</head>
+
+<body>
+	<main>
+		<nav>
+			<div class="nav-inner">
+				<div class="left">
+					<div class="logo"></div>
+
+					<p class="name">CodeWithArhan</p>
+
+					<a href="/blog.php">Blog</a>
+
+					<a href="/courses.php">Courses</a>
+
+					<a href="/tutorials.php">Tutorials</a>
+
+					<a href="/about.php">About</a>
+				</div>
+				</div>
+			</div>
+		</nav>
+		<iframe width="875" height="509" src="https://www.youtube.com/embed/Tto8TS-fJQU" title="Some Amazing Python Programs - The Power of Python | Python Tutorial - Day #2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+		<br>
+		<br>
+		<section class="call-to-action">
+			<div class="callout-container">
+				<h2><b>Day 2 - My Python Success Story <br>
+				Why I love Python (And you will too...)</b></h2>
+				<p>Welcome to Day 2 of 100 days of code. Let me start with a story! Back in my college, I used to learn C and C++ programming in depth, used to score good marks. I created a bunch of printing, conditionals and loop program. Now what? I wanted to benefit from the same In my second year of college, I started working (I mean actually working in the industry) with the python programming language. I was not so good with it but I used to write code for a singaporean client and actually make good money without having to actually master Python. Harry then got curious and started working on his Python skills even more. I then got into web scraping and trust me I made some good easy money on Fiverr just by writing some python programs and charging on per webpage basis to my clients ( I used to automate scraping)
+					<br>
+
+					I then learnt flask and got to work with Flask with a university professor abroad. Long story short, Python made a huge impact in my career.</p>
+				<h2><b>What can Python do for you?</b></h2>
+				<p>I want to show you some Python programs I created which will surely inspire you to create your own versions of the same as we progress through this tutorial. Do not try to recreate them just yet if you are a beginner and just started working on Python. We will make progress gradually trust me.</p>
+				<p>In this tutorial, we will use Replit to create a Python program</p>
+
+				<h2><b>Installing Python on Windows</b></h2>
+				<p>If you are not using replit for some reason, you can install python on your windows or mac machine. If you are using windows, here are the exact steps to install python: <br></p>
+				<ul>
+				<li>Step 1: Go to Google and search for "Download python". Click on the first link</li>
+					<img src="https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/videos/python-100-days-of-code-2/search-download-python-on-google.png" width="50%" height="60%">
+					<li>Step 2: Download the latest version of Python from python.org website</li>
+					<img
+						src="https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/videos/python-100-days-of-code-2/python-download-from-python-org-website.png" width="50%" height="60%">
+					<li>Step 3: Open the downloaded file to install Python. Make sure you check the arrow beside "Add Python to Path". This step is very important</li>
+					<img
+						src="https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/videos/python-100-days-of-code-2/add-python-to-path.png" width="50%" height="60%">
+					<li>Step 4: Click "Install Now" and Python will start to install</li>
+					<img
+						src="https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/videos/python-100-days-of-code-2/python-is-installing.png" width="50%" height="60%">
+					<li>Step 5: Once the setup ends, close the window. Now open your terminal or Powershell and type python followed by enter key. You should see a screen like this:</li>
+					<img
+						src="https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/videos/python-100-days-of-code-2/python-installed.png" width="50%" height="60%">
+			</ul>
+			<p>The steps are very similar for Mac and Linux users as well. Simply download the installer and follow the steps shown above. </p>
+
+	<iframe height="400px" width="100%" src="https://replit.com/@arhanansari2009/02-Day2-Application-of-Python?lite=true" scrolling="no"frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
+				<button class="cta" onclick="location.href='/courses/100-days-python/day1.php'">Previous</button>
+				<button class="cta" onclick="location.href='/courses/100-days-python/day3.php'">Next>></button>
+			</div>
+		</section>
+	</main>
+</body>
+
+</html>
